c8cf440c901bad3417a69bfc02aa4642f633d2a0
fedulov <fedulov@local>
2026-07-01 11:07:20 +0300

    Revert "feat: request fullscreen on webapp startup"
    This reverts commit 13b48a831442dcd21e67c8725268f23181c01e1c.

--- webapp-react/src/main.jsx
+++ webapp-react/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/app.css';
-import { tryEnterTelegramFullscreen } from './lib/telegram.js';
-
-tryEnterTelegramFullscreen();
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
